b03bd85d9be467aa64f40499d6d0bd5c929195f0
Alexander Lebedev <lebedev@0x4e3.ru>
2017-09-16 20:40:12 +0300

    Adds tests for 'init' command

--- tests/test_aperitive.py
+++ tests/test_aperitive.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `aperitive` package."""
+import os
 
 import pytest
+import yaml
 
 from click.testing import CliRunner
 
@@ -35,3 +37,31 @@ def test_command_line_interface():
     help_result = runner.invoke(console.cli, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_init():
+    runner = CliRunner()
+
+    command_input = 'gitlab.com\napiuser\npwd\npwd' \
+                    '\nredmine.com\napiuser\npwd\npwd'
+
+    # Test 'init' command in the isolated filesystem and
+    # custom config directory.
+    with runner.isolated_filesystem():
+        config_dir = './config'
+        config_file = os.path.join(config_dir, '.aperitive.yml')
+
+        result = runner.invoke(
+            console.cli,
+            ['init', config_dir],
+            input=command_input + '\ny')
+        assert not result.exception
+
+        assert os.path.exists(config_dir) and os.path.isdir(config_dir)
+
+        assert os.path.exists(config_file) and os.path.isfile(config_file)
+
+        with open(config_file) as f:
+            config = yaml.load(f)
+            assert config['gitlab.server'] == 'gitlab.com'
+            assert config['redmine.server'] == 'redmine.com'
